7fff731997d696e417bf3086a22e054d7af2fab1
vahid-nejad <vahid884@gmail.com>
2022-11-21 08:51:11 +0330

    first commit

---
+++ dummyContents.ts
@@ -0,0 +1,27 @@
+import { Product } from "./interfaces";
+export const products: Product[] = [
+  {
+    id: 1,
+    imageUrl: "/vercel.svg",
+    name: "Product1",
+    price: 13,
+  },
+  {
+    id: 2,
+    imageUrl: "/vercel.svg",
+    name: "Product1",
+    price: 13,
+  },
+  {
+    id: 3,
+    imageUrl: "/vercel.svg",
+    name: "Product1",
+    price: 13,
+  },
+  {
+    id: 4,
+    imageUrl: "/vercel.svg",
+    name: "Product1",
+    price: 13,
+  },
+];
